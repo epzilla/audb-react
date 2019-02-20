bf919b45100a36ca52de01341d43df2b4d8fe316
Adam Epling <adamepling@gmail.com>
2019-02-19 21:16:30 -0600

    signup page

--- client/src/routes/signup.tsx
+++ client/src/routes/signup.tsx
@@ -1,78 +1,75 @@
-import { h, Component } from 'react';
+import React, { FC, useState, useRef } from 'react';
 import Rest from '../lib/rest-service';
-import { route } from 'react-router';
-import { Link } from 'react-router/match';
+import { Link } from 'react-router-dom';
 import LocalStorageService from '../lib/local-storage-service';
+import { createBrowserHistory } from 'history';
 
-export default class Signup extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { name: '', email: '', password: '', error: null };
-    if (props && props.user) {
-      let redirect = props.returnUrl ? props.returnUrl : '';
-      route(`/${redirect}`);
-    }
+const history = createBrowserHistory();
+
+interface ISignupProps {
+  user?: any;
+  config?: any;
+  returnUrl?: string;
+  loginCb?: Function;
+}
+
+export const Signup: FC<ISignupProps> = (props) => {
+  const { config, loginCb, returnUrl, user } = props;
+  if (user) {
+    let redirect = returnUrl ? returnUrl : '';
+    history.push(`/${redirect}`);
   }
 
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.user) {
-      let redirect = this.props.returnUrl ? this.props.returnUrl : '';
-      route(`/${redirect}`);
-    }
-  }
+  const nameRef = useRef<HTMLInputElement>(null);
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+  const [error, setError] = useState('');
 
-  setValue = (e) => {
-    let obj = {};
-    obj[e.target.name] = e.target.value;
-    this.setState(obj);
-  };
-
-  submit = (e) => {
+  const onSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    Rest.post('users', this.state).then(user => {
+    Rest.post('users', {
+      name: nameRef.current.value,
+      email: emailRef.current.value,
+      password: passwordRef.current.value
+    }).then(user => {
       LocalStorageService.set('user', user);
-      if (this.props.loginCb) {
-        this.props.loginCb(user);
+      if (loginCb) {
+        loginCb(user);
       }
 
-      let redirct = (this.props && this.props.returnUrl) ? this.props.returnUrl : '';
-      route(`/${redirct}`);
-    }).catch(err => {
-      this.setState({ error: err });
-    });
+      let redirct = (props && returnUrl) ? returnUrl : '';
+      history.push(`/${redirct}`);
+    }).catch(e =>  setError(e));
   };
 
-  render() {
-    let errBlock = this.state.error ? (
-      <div className="form-group has-error">
-        <p className="help-block">Hmmm... either your email or password isn't right. Please try again.</p>
-      </div>
-    ) : null;
-
-    return (
-      <div className="main login">
-        <h1>Sign Up</h1>
-        <form name="form" onSubmit={(e) => this.submit(e)}>
-          <div className="form-group">
-            <label>Name</label>
-            <input type="text" name="name" autofocus="autofocus" placeholder={this.props.config.signupPlaceholderName} className="form-control" onChange={this.setValue} />
-          </div>
-          <div className="form-group">
-            <label>Email</label>
-            <input type="email" name="email" placeholder={this.props.config.signupPlaceholderEmail} className="form-control" onChange={this.setValue} />
-          </div>
-          <div className="form-group">
-            <label>Password</label>
-            <input type="password" name="password" placeholder={this.props.config.signupPlaceholderPW} className="form-control" onChange={this.setValue} />
-          </div>
-          { errBlock }
-          <button type="submit" className="btn primary"> Sign Up </button><span className="clearfix"></span>
-        </form>
-        <div className="row pad-mobile">
-            <hr/>
-            Already have an account? <Link href="/login" className="text-center new-account">Log in.</Link>
+  return (
+    <div className="main login">
+      <h1>Sign Up</h1>
+      <form name="form" onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>Name</label>
+          <input type="text" name="name" autoFocus placeholder={config.signupPlaceholderName} className="form-control" />
         </div>
+        <div className="form-group">
+          <label>Email</label>
+          <input type="email" name="email" placeholder={config.signupPlaceholderEmail} className="form-control" />
+        </div>
+        <div className="form-group">
+          <label>Password</label>
+          <input type="password" name="password" placeholder={config.signupPlaceholderPW} className="form-control" />
+        </div>
+        {
+          error &&
+          <div className="form-group has-error">
+            <p className="help-block">Hmmm... something went wrong. Please try again.</p>
+          </div>
+        }
+        <button type="submit" className="btn primary"> Sign Up </button><span className="clearfix"></span>
+      </form>
+      <div className="row pad-mobile">
+        <hr />
+        Already have an account? <Link to="/login" className="text-center new-account">Log in.</Link>
       </div>
-    );
-  }
+    </div>
+  );
 }
