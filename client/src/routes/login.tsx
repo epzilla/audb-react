6705a95d0083e4acce56a927692463365c2250e3
Adam Epling <adamepling@gmail.com>
2019-02-19 20:56:23 -0600

    login page

--- client/src/routes/login.tsx
+++ client/src/routes/login.tsx
@@ -1,73 +1,66 @@
-import { h, Component } from 'react';
+import React, { FC, useState, useRef } from 'react';
 import Rest from '../lib/rest-service';
-import { Link } from 'react-router/match';
+import { Link } from 'react-router-dom';
 import LocalStorageService from '../lib/local-storage-service';
 
-export default class Login extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { email: '', password: '', error: null };
-    if (props && props.user) {
-      let redirect = props.returnUrl ? props.returnUrl : '';
-      window.location.assign(`/${redirect}`);
-    }
+interface ILoginProps {
+  user: any;
+  config: any;
+  returnUrl?: string;
+  loginCb?: Function;
+}
+
+export const Login: FC<ILoginProps> = (props) => {
+  const { user, returnUrl, config, loginCb } = props;
+  if (user) {
+    let redirect = returnUrl ? returnUrl : '';
+    window.location.assign(`/${redirect}`);
   }
 
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.user) {
-      let redirect = this.props.returnUrl ? this.props.returnUrl : '';
-      window.location.assign(`/${redirect}`);
-    }
-  }
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+  const [error, setError] = useState('');
 
-  setValue = (e) => {
-    let obj = {};
-    obj[e.target.name] = e.target.value;
-    this.setState(obj);
-  };
-
-  submit = (e) => {
+  const onSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    Rest.post('session', this.state).then(user => {
+    Rest.post('session', {
+      email: emailRef.current.value,
+      password: passwordRef.current.value
+    }).then(user => {
       LocalStorageService.set('user', user);
-      if (this.props.loginCb) {
-        this.props.loginCb(user);
+      if (loginCb) {
+        loginCb(user);
       }
 
-      let redirct = (this.props && this.props.returnUrl) ? this.props.returnUrl : '';
-      window.location.assign(`/${redirct}`);
-    }).catch(err => {
-      this.setState({ error: err });
-    });
+      let redirect = (props && returnUrl) ? returnUrl : '';
+      window.location.assign(`/${redirect}`);
+    }).catch(e => setError(e));
   };
 
-  render() {
-    let errBlock = this.state.error ? (
-      <div className="form-group has-error">
-        <p className="help-block">{ this.state. error }</p>
-      </div>
-    ) : null;
-
-    return (
-      <div className="main login">
-        <h1>Login</h1>
-        <form name="form" onSubmit={(e) => this.submit(e)}>
-          <div className="form-group">
-            <label>Email</label>
-            <input type="email" name="email" autofocus="autofocus" placeholder={this.props.config.loginPlaceholderEmail} className="form-control" onChange={this.setValue} />
-          </div>
-          <div className="form-group">
-            <label>Password</label>
-            <input type="password" name="password" placeholder={this.props.config.loginPlaceholderPW} className="form-control" onChange={this.setValue} />
-          </div>
-          { errBlock }
-          <button type="submit" className="btn primary"> Sign in </button><span className="clearfix"></span>
-        </form>
-        <div className="row pad-mobile">
-            <hr/>
-            Not registered? <Link href="/sign-up" className="text-center new-account">Create an account.</Link>
+  return (
+    <div className="main login">
+      <h1>Login</h1>
+      <form name="form" onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>Email</label>
+          <input ref={emailRef} type="email" name="email" autoFocus placeholder={config.loginPlaceholderEmail} className="form-control" />
         </div>
+        <div className="form-group">
+          <label>Password</label>
+          <input ref={passwordRef} type="password" name="password" placeholder={config.loginPlaceholderPW} className="form-control" />
+        </div>
+        {
+          error &&
+          <div className="form-group has-error">
+            <p className="help-block">{error}</p>
+          </div>
+        }
+        <button type="submit" className="btn primary"> Sign in </button><span className="clearfix"></span>
+      </form>
+      <div className="row pad-mobile">
+        <hr />
+        Not registered? <Link to="/sign-up" className="text-center new-account">Create an account.</Link>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
