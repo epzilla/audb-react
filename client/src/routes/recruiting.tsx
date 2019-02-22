233f910814e69d63183450f88446ef5401321106
Adam Epling <adamepling@gmail.com>
2019-02-21 22:04:05 -0600

    Recruiting page

--- client/src/routes/recruiting.tsx
+++ client/src/routes/recruiting.tsx
@@ -1,256 +1,253 @@
-import { h, Component } from 'react';
+import { FC, useState, useRef } from 'react';
 import Rest from '../lib/rest-service';
 import LocalStorageService from '../lib/local-storage-service';
+import config from '../config';
 
-export default class Recruiting extends Component {
-  constructor(props) {
-    super(props);
-    this.years = [];
-    this.columns = [
-      { field: 'lname', name: 'Name'},
-      { field: 'pos', name: 'Pos'},
-      { field: 'state', name: 'Hometown'},
-      { field: 'hs', name: 'School', hideSmall: true},
-      { field: 'rivalsStars', name: 'Rivals Stars', hideSmall: true, center: true},
-      { field: 'scoutStars', name: 'Scout Stars', hideSmall: true, center: true},
-      { field: 'rivalsRank', name: 'Rivals Rank', hideSmall: true, center: true},
-      { field: 'scoutRank', name: 'Scout Rank', hideSmall: true, center: true},
-      { field: 'avgStars', name: 'Stars', hideLarge: true, center: true},
-      { field: 'avgRank', name: 'Rank', hideLarge: true, center: true}
-    ];
+const years = [];
+const columns = [
+  { field: 'lname', name: 'Name'},
+  { field: 'pos', name: 'Pos'},
+  { field: 'state', name: 'Hometown'},
+  { field: 'hs', name: 'School', hideSmall: true},
+  { field: 'rivalsStars', name: 'Rivals Stars', hideSmall: true, center: true},
+  { field: 'scoutStars', name: 'Scout Stars', hideSmall: true, center: true},
+  { field: 'rivalsRank', name: 'Rivals Rank', hideSmall: true, center: true},
+  { field: 'scoutRank', name: 'Scout Rank', hideSmall: true, center: true},
+  { field: 'avgStars', name: 'Stars', hideLarge: true, center: true},
+  { field: 'avgRank', name: 'Rank', hideLarge: true, center: true}
+];
 
-    this.maps = this.props.config.recruitMaps;
+const maps = config.recruitMaps;
 
-    let d = new Date();
-    this.currentYear = d.getMonth() < 2 ? d.getFullYear() : d.getFullYear() + 1;
-    this.firstTrackedClass = this.props.config.firstTrackedRecruitingSeason || this.currentYear;
+let d = new Date();
+const currentYear = d.getMonth() < 2 ? d.getFullYear() : d.getFullYear() + 1;
+const firstTrackedClass = config.firstTrackedRecruitingSeason || currentYear;
 
-    for (let i = this.currentYear + 2; i >= this.firstTrackedClass; i--) {
-      this.years.push(i);
-    }
+for (let i = currentYear + 2; i >= firstTrackedClass; i--) {
+  years.push(i);
+}
 
-    this.state = {
-      year: this.currentYear,
-      recruits: [],
-      sortCol: null,
-      sortAsc: true
-    };
+export const RecruitingView: FC = () => {
+  const [year, setYear] = useState(currentYear);
+  const [recruits, setRecruits] = useState([]);
+  const [sortCol, setSortCol] = useState('lname');
+  const [sortAsc, setSortAsc] = useState(true);
+  const inputRef = useRef<HTMLInputElement>(null);
 
-    this.getRecruits(this.currentYear);
-  }
-
-  componentDidMount() {
-    document.removeEventListener('keyup', this.onKeyUp);
-    document.addEventListener('keyup', this.onKeyUp);
-  }
-
-  componentWillUnmount() {
-    document.removeEventListener('keyup', this.onKeyUp);
-  }
-
-  onKeyUp = ({ key, target, shiftKey }) => {
+  const onKeyUp = ({ key, target, shiftKey }) => {
     if (target.nodeName !== 'INPUT') {
       switch (key) {
         case '/':
-          let input = document.querySelector('.main.recruiting input');
-          if (input) {
-            input.focus();
+          if (inputRef) {
+            inputRef.current.focus();
           }
           break;
         case 'ArrowLeft':
-          this.prev();
+          prev();
           break;
         case 'ArrowRight':
-          this.next();
+          next();
           break;
         case 'n':
-          this.sortCol('lname');
+          sortByCol('lname');
           break;
         case 'p':
-          this.sortCol('pos');
+          sortByCol('pos');
           break;
         case 'h':
-          this.sortCol('hs');
+          sortByCol('hs');
           break;
         case 'H':
-          this.sortCol('state')
+          sortByCol('state')
           break;
         case 'r':
-          this.sortCol('rivalsStars');
+          sortByCol('rivalsStars');
           break;
         case 'R':
-          this.sortCol('rivalsRank');
+          sortByCol('rivalsRank');
           break;
         case 's':
-          this.sortCol('scoutStars');
+          sortByCol('scoutStars');
           break;
         case 'S':
-          this.sortCol('scoutRank');
+          sortByCol('scoutRank');
           break;
       }
     }
   };
 
-  onChange = (e) => {
+  const onChange = (e) => {
     if (e && e.target.value) {
-      this.changeYear(parseInt(e.target.value));
+      changeYear(parseInt(e.target.value));
     }
   };
 
-  changeYear = (year) => {
-    this.setState({ year });
-    this.getRecruits(year);
+  const changeYear = (y) => {
+    setYear(y);
+    getRecruits(year);
   };
 
-  getRecruits = (year) => {
-    let recruits = LocalStorageService.get(`rec-${year}`);
-    if (recruits) {
-      this.setState({ recruits: recruits });
+  const getRecruits = (recruitYear) => {
+    let recs = LocalStorageService.get(`rec-${recruitYear}`);
+    if (recs) {
+      setRecruits(recs);
     }
 
     Rest.get(`recruits/${year}`).then(res => {
-      this.setState({ recruits: res });
+      setRecruits(res);
       if (JSON.stringify(res) !== JSON.stringify(recruits)) {
         LocalStorageService.set(`rec-${year}`, res);
       }
     });
   };
 
-  sortCol = (field) => {
-    let col = this.columns.find(c => c.field === field);
+  const sortByCol = (field) => {
+    let col = columns.find(c => c.field === field);
     if (col) {
-      let currentSortField = this.state.sortCol;
+      let currentSortField = sortCol;
       if (currentSortField === col.field) {
         // We are just re-sorting the already chosed column
-        this.setState({ sortAsc: !this.state.sortAsc });
+        setSortAsc(!sortAsc);
       } else {
-        this.setState({ sortCol: col.field });
+        setSortCol(col.field);
       }
     }
   };
 
-  next = () => {
-    let year = this.state.year + 1;
-    if (year <= this.currentYear + 2) {
-      this.changeYear(year);
+  const next = () => {
+    let nextYear = year + 1;
+    if (nextYear <= currentYear + 2) {
+      changeYear(nextYear);
     }
   };
 
-  prev = () => {
-    let year = this.state.year - 1;
-    if (year >= this.props.config.firstSeason) {
-      this.changeYear(year);
+  const prev = () => {
+    let prevYear = year - 1;
+    if (prevYear >= config.firstSeason) {
+      changeYear(prevYear);
     }
   };
 
-  render() {
-    let self = this;
-    let table;
-    let rows = [];
-    let headers = [];
-    let year = this.state.year;
-    let map;
-    let sortCol = this.state.sortCol || 'lname';
-    let recruits = this.state.recruits;
+  let headers = [];
 
-    recruits.sort((a, b) => {
-      if (!a[sortCol] || !b[sortCol]) {
-        return 0;
-      }
-
-      if (typeof a[sortCol] === 'string') {
-        let nameA = a[sortCol].toUpperCase();
-        let nameB = b[sortCol].toUpperCase();
-        if (nameA < nameB) {
-          return this.state.sortAsc ? -1 : 1;
-        }
-        if (nameA > nameB) {
-          return this.state.sortAsc ? 1 : -1;
-        }
-        return 0;
-      }
-
-      if (typeof a[sortCol] === 'number') {
-        return this.state.sortAsc ? (a[sortCol] - b[sortCol]) : (b[sortCol] - a[sortCol]);
-      }
-
+  recruits.sort((a, b) => {
+    if (!a[sortCol] || !b[sortCol]) {
       return 0;
-    });
+    }
 
-    rows = this.state.recruits.map(rec => {
-      return (
-        <tr>
-          <td>{`${rec.fname} ${rec.lname}`}</td>
-          <td>{rec.pos}</td>
-          <td>{`${rec.city}, ${rec.state}`}</td>
-          <td className="larger">{rec.hs}</td>
-          <td className="larger center">{`${rec.rivalsStars} ★`}</td>
-          <td className="larger center">{`${rec.scoutStars} ★`}</td>
-          <td className="larger center">{rec.rivalsRank}</td>
-          <td className="larger center">{rec.scoutRank}</td>
-          <td className="smaller center">{`${rec.avgStars} ★`}</td>
-          <td className="smaller center">{rec.avgRank}</td>
-        </tr>
-      );
-    });
-
-    headers = this.columns.map(col => {
-      let arrow;
-      let className = col.field;
-
-      if (col.center) {
-        className += ' center';
+    if (typeof a[sortCol] === 'string') {
+      let nameA = a[sortCol].toUpperCase();
+      let nameB = b[sortCol].toUpperCase();
+      if (nameA < nameB) {
+        return sortAsc ? -1 : 1;
       }
-
-      if (col.hideSmall) {
-        className += ' larger';
+      if (nameA > nameB) {
+        return sortAsc ? 1 : -1;
       }
-      else if (col.hideLarge) {
-        className += ' smaller';
-      }
+      return 0;
+    }
 
-      if (this.state.sortCol === col.field) {
-        className += ' sort';
-        className += (this.state.sortAsc ? 'asc' : 'desc');
+    if (typeof a[sortCol] === 'number') {
+      return sortAsc ? (a[sortCol] - b[sortCol]) : (b[sortCol] - a[sortCol]);
+    }
 
-        arrow = this.state.sortAsc ? <span className="sort-arrow">▲</span> : <span className="sort-arrow">▼</span>;
-      }
+    return 0;
+  });
 
-      return (<th className={className} onClick={() => self.sortCol(col.field)}>{col.name}{arrow}</th>);
-    });
+  headers = columns.map(col => {
+    let arrow;
+    let className = col.field;
 
-    table = (
+    if (col.center) {
+      className += ' center';
+    }
+
+    if (col.hideSmall) {
+      className += ' larger';
+    }
+    else if (col.hideLarge) {
+      className += ' smaller';
+    }
+
+    if (sortCol === col.field) {
+      className += ' sort';
+      className += (sortAsc ? 'asc' : 'desc');
+
+      arrow = sortAsc ? <span className="sort-arrow">▲</span> : <span className="sort-arrow">▼</span>;
+    }
+
+    return (<th className={className} onClick={() => sortByCol(col.field)}>{col.name}{arrow}</th>);
+  });
+
+  return (
+    <div className="main recruiting">
+      <h1>{ config.team } Football Recruiting</h1>
+      <select className="big-select margin-bottom-1rem" onChange={onChange}>
+        {
+          years.map(y => <option value={y} selected={y === year}>{y}</option>)
+        }
+      </select>
       <table className="recruits-table">
         <thead>
           { headers }
         </thead>
         <tbody>
-          { rows }
+          {
+            recruits.map(rec => {
+              return (
+                <tr>
+                  <td>{`${rec.fname} ${rec.lname}`}</td>
+                  <td>{rec.pos}</td>
+                  <td>{`${rec.city}, ${rec.state}`}</td>
+                  <td className="larger">{rec.hs}</td>
+                  <td className="larger center">{`${rec.rivalsStars} ★`}</td>
+                  <td className="larger center">{`${rec.scoutStars} ★`}</td>
+                  <td className="larger center">{rec.rivalsRank}</td>
+                  <td className="larger center">{rec.scoutRank}</td>
+                  <td className="smaller center">{`${rec.avgStars} ★`}</td>
+                  <td className="smaller center">{rec.avgRank}</td>
+                </tr>
+              );
+            })
+          }
         </tbody>
       </table>
-    );
-
-    if (this.maps[year]) {
-      map = (
+      {
+        maps[year] &&
         <div className="align-center full-width">
-          <iframe src={this.maps[year]}></iframe>
+          <iframe src={maps[year]}></iframe>
           <br />
-          <small>View <a href={this.maps[year]} target="_blank">{ year } { this.props.config.team } Football Commitments</a> in a larger map</small>
+          <small>View <a href={maps[year]} target="_blank">{ year } { config.team } Football Commitments</a> in a larger map</small>
         </div>
-      );
-    }
+      }
+    </div>
+  );
+};
 
-    return (
-      <div className="main recruiting">
-        <h1>{ this.props.config.team } Football Recruiting</h1>
-        <select className="big-select margin-bottom-1rem" onChange={this.onChange}>
-          {
-            this.years.map(y => <option value={y} selected={y === this.state.year}>{y}</option>)
-          }
-        </select>
-        { table }
-        { map }
-      </div>
-    );
-  }
-}
+
+// export default class Recruiting extends Component {
+
+//     state = {
+//       year: currentYear,
+//       recruits: [],
+//       sortCol: null,
+//       sortAsc: true
+//     };
+
+//     getRecruits(currentYear);
+//   }
+
+//   componentDidMount() {
+//     document.removeEventListener('keyup', onKeyUp);
+//     document.addEventListener('keyup', onKeyUp);
+//   }
+
+//   componentWillUnmount() {
+//     document.removeEventListener('keyup', onKeyUp);
+//   }
+
+
+
+//   render() {
+
+// }
